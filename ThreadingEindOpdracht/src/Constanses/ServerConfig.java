69ad8d5ced4f9550d606928fcb1a362885add388
leonvantuyl <leonvtuyl@hotmail.com>
2014-06-04 15:34:36 +0200

    laden html pagina

--- ThreadingEindOpdracht/src/Constanses/ServerConfig.java
+++ ThreadingEindOpdracht/src/Constanses/ServerConfig.java
@@ -7,10 +7,16 @@ public class ServerConfig
     //public static final int serverIp = localhost;
     public static int serverPort = 8000;
     public static int controlPort = 8001;
+    public static String webRoot = "c:/webserver/webroot";
+    public static String defaultpage = "index.html";
+    public static Boolean directoryBrowsing = true;
+    
     
     //database info
     public static final String databaseHost = "jdbc:mysql://databases.aii.avans.nl/";
     public static final String databaseName = "gagpvenn_db2";
     public static final String databaseUsername = "gagpvenn";
     public static final String databasePassword = "123abc456789"; 
+    
+
   }
--- ThreadingEindOpdracht/src/Model/ControlServer/ControlServerHandler.java
+++ ThreadingEindOpdracht/src/Model/ControlServer/ControlServerHandler.java
@@ -1,62 +1,63 @@
 package Model.ControlServer;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Date;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ControlServerHandler implements Runnable
-  {
+public class ControlServerHandler implements Runnable {
 
     private final Socket socket;
     private String userRequestUrl;
 
-    public ControlServerHandler(Socket socket)
-      {
+    public ControlServerHandler(Socket socket) {
         this.socket = socket;
-      }
+    }
 
     @Override
-    public void run()
-      {
+    public void run() {
         System.out.println("new control server");
-
-        readHeader();
-      }
-
-    private void readHeader()
-      {
-        BufferedReader bufReader = null;
-        try
-          {
-            bufReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String inputLine;
-            while (!(inputLine = bufReader.readLine()).equals(""))
-              {
-                if (inputLine.startsWith("Host: "))
-                  {
-                    userRequestUrl = inputLine.replaceAll("Host: ", "");
-                  }
-              }
-            bufReader.close();
-          }
-        catch (IOException ex)
-          {
+        try {
+            readHeader();
+            //TODO verwijs naar de goede pagina   
+            
+            File file = new File( new java.io.File("").getAbsolutePath() + "\\src\\View\\ControlPanel.html");
+            Loadpage(file);
+            socket.close();
+        } catch (IOException ex) {
             Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-          }
-        finally
-          {
-            try
-              {
-                bufReader.close();
-              }
-            catch (IOException ex)
-              {
-                Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-              }
-          }
-      }
-  }
+        }
+
+    }
+
+    private void readHeader() throws IOException {
+        BufferedReader bufReader = null;
+        bufReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String inputLine;
+        while (!(inputLine = bufReader.readLine()).equals("")) {
+            if (inputLine.startsWith("Host: ")) {
+                userRequestUrl = inputLine.replaceAll("Host: ", "");
+            }
+        }
+        socket.isInputShutdown();
+    }
+
+    private boolean Loadpage(File file) {
+        try {
+            //TODO check wat de delimiter doet
+           String text = new Scanner(file).useDelimiter("\\A").next();
+           PrintWriter out = new PrintWriter(socket.getOutputStream());
+           out.write(text);
+           out.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+}
