81ce78abb0bf43ee637626dbe12c136c3e62f103
Gijs van der Venne <gijs.venne@hotmail.com>
2014-06-05 00:25:15 +0200

    ombouwing url handling

--- ThreadingEindOpdracht/src/Model/ControlServer/ControlServerHandler.java
+++ ThreadingEindOpdracht/src/Model/ControlServer/ControlServerHandler.java
@@ -10,54 +10,91 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ControlServerHandler implements Runnable {
+public class ControlServerHandler implements Runnable
+  {
 
     private final Socket socket;
     private String userRequestUrl;
 
-    public ControlServerHandler(Socket socket) {
+    public ControlServerHandler(Socket socket)
+      {
         this.socket = socket;
-    }
+      }
 
     @Override
-    public void run() {
+    public void run()
+      {
         System.out.println("new control server");
-        try {
+        try
+          {
             readHeader();
-            //TODO verwijs naar de goede pagina   
-            
-            File file = new File( new java.io.File("").getAbsolutePath() + "\\src\\View\\ControlPanel.html");
-            Loadpage(file);
+
+            switch (userRequestUrl)
+              {
+                case "/":
+                  {
+                    File file = new File(new java.io.File("").getAbsolutePath() + "\\src\\View\\login.html");
+                    Loadpage(file);
+                  }
+                case "/login":
+                  {
+                    //check login
+                    File file = new File(new java.io.File("").getAbsolutePath() + "\\src\\View\\ControlPanel.html");
+                    Loadpage(file);
+                  }
+                default:
+                  {
+                    File file = new File(new java.io.File("").getAbsolutePath() + "\\src\\View\\error\\404.html");
+                    Loadpage(file);
+                  }
+              }
+
             socket.close();
-        } catch (IOException ex) {
+          }
+        catch (IOException ex)
+          {
             Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
+          }
 
-    }
+      }
 
-    private void readHeader() throws IOException {
+    private void readHeader() throws IOException
+      {
         BufferedReader bufReader = null;
         bufReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String inputLine;
-        while (!(inputLine = bufReader.readLine()).equals("")) {
-            if (inputLine.startsWith("Host: ")) {
-                userRequestUrl = inputLine.replaceAll("Host: ", "");
-            }
-        }
+        while (!(inputLine = bufReader.readLine()).equals(""))
+          {
+            if (inputLine.startsWith("GET"))
+              {
+                String[] parts = inputLine.split(" ");
+                
+                for(String part : parts)
+                  {
+                      System.out.println(part);
+                  }
+                
+                userRequestUrl = parts[1];
+              }
+          }
         socket.isInputShutdown();
-    }
+      }
 
-    private boolean Loadpage(File file) {
-        try {
+    private boolean Loadpage(File file)
+      {
+        try
+          {
             //TODO check wat de delimiter doet
-           String text = new Scanner(file).useDelimiter("\\A").next();
-           PrintWriter out = new PrintWriter(socket.getOutputStream());
-           out.write(text);
-           out.close();
+            String text = new Scanner(file).useDelimiter("\\A").next();
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            out.write(text);
+            out.close();
 
-        } catch (IOException ex) {
+          }
+        catch (IOException ex)
+          {
             Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
+          }
         return false;
-    }
-}
+      }
+  }
