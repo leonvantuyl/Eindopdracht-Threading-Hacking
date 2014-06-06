1bb131ffe2ad185010f8b00785dfcf0ddf534a3e
Gijs van der Venne <gijs.venne@hotmail.com>
2014-06-06 16:22:59 +0200

    request body komt in code

--- ThreadingEindOpdracht/src/Model/ControlServer/ControlServerHandler.java
+++ ThreadingEindOpdracht/src/Model/ControlServer/ControlServerHandler.java
@@ -8,110 +8,145 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.CharBuffer;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ControlServerHandler implements Runnable {
+public class ControlServerHandler implements Runnable
+  {
 
     private final Socket socket;
     private String userRequestUrl;
     private String data;
 
-    public ControlServerHandler(Socket socket) {
+    public ControlServerHandler(Socket socket)
+      {
         this.socket = socket;
-    }
+      }
 
     @Override
-    public void run() {
+    public void run()
+      {
         System.out.println("new control server");
-        try {
+        try
+          {
             readHeader();
 
-            switch (userRequestUrl) {
-                case "/": {
+            switch (userRequestUrl)
+              {
+                case "/":
+                  {
                     File file = new File(new java.io.File("").getAbsolutePath() + "\\src\\View\\login.html");
                     Loadpage(file);
-                }
+                  }
                 break;
-                case "/controlPanel": {
+                case "/controlPanel":
+                  {
                     //TODO check login
                     File file = new File(new java.io.File("").getAbsolutePath() + "\\src\\View\\ControlPanel.html");
                     Loadpage(file);
-                }
+                  }
                 break;
-                case "/controlPanel/config": {
+                case "/controlPanel/config":
+                  {
                     sendConfigInfo();
-                }
+                  }
                 break;
-                case "/controlPanel/configPost": {
+                case "/controlPanel/configPost":
+                  {
                     postConfigInfo();
-                }
+                  }
                 break;
-                default: {
+                default:
+                  {
                     File file = new File(new java.io.File("").getAbsolutePath() + "\\src\\View\\error\\404.html");
                     Loadpage(file);
-                }
+                  }
                 break;
-            }
+              }
 
             socket.close();
-        } catch (IOException ex) {
+          }
+        catch (IOException ex)
+          {
             Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
+          }
 
-    }
+      }
 
-    private void readHeader() throws IOException {
+    private void readHeader() throws IOException
+      {
         BufferedReader bufReader = null;
-        InputStream is = socket.getInputStream();        
+        InputStream is = socket.getInputStream();
         bufReader = new BufferedReader(new InputStreamReader(is));
         String inputLine;
+        StringBuilder rawBody = new StringBuilder();
+        int contentLength = 0;
+        boolean done = false;
 
-        while (!(inputLine = bufReader.readLine()).equals("")) {
+        while (!(inputLine = bufReader.readLine()).equals(""))
+          {
             System.out.println(inputLine);
-            if (inputLine.startsWith("GET") || inputLine.startsWith("POST")) {
-
+            if (inputLine.startsWith("GET") || inputLine.startsWith("POST"))
+              {
                 String[] parts = inputLine.split(" ");
-
-                for (String part : parts) {
-            //        System.out.println(part);
-                }
-
                 userRequestUrl = parts[1];
+              }
 
-            }
-        }
-        socket.isInputShutdown();
-    }
+            else if (inputLine.startsWith("Content-Length: "))
+              { // get the content-length
+                int index = inputLine.indexOf(':') + 1;
+                String len = inputLine.substring(index).trim();
+                contentLength = Integer.parseInt(len);
+              }
+          }
+        byte[] bytes = new byte[contentLength];
+        for (int i = 0; i < contentLength; i++)
+          {
+            bytes[i] = (byte) bufReader.read();
+          }
+        String body = new String(bytes);
+          System.out.println(body);
+        
+      }
 
-    private void Loadpage(File file) {
-        try {
+    private void Loadpage(File file)
+      {
+        try
+          {
             String text = new Scanner(file).useDelimiter("\\A").next();
             PrintWriter out = new PrintWriter(socket.getOutputStream());
             out.write(text);
             out.close();
 
-        } catch (IOException ex) {
+          }
+        catch (IOException ex)
+          {
             Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+          }
+      }
 
-    private void sendConfigInfo() {
-        try {
+    private void sendConfigInfo()
+      {
+        try
+          {
             //TODO check wat de delimiter doet
             String text = ServerConfig.getInfo();
             PrintWriter out = new PrintWriter(socket.getOutputStream());
             out.write(text);
             out.close();
 
-        } catch (IOException ex) {
+          }
+        catch (IOException ex)
+          {
             Logger.getLogger(ControlServerHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+          }
+      }
 
-    private void postConfigInfo() {
+    private void postConfigInfo() throws IOException
+      {
         System.out.println("yo");
-    }
+      }
 
-}
+  }
