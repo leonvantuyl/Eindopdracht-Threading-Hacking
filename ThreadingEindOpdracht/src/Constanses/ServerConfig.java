d651caf1399df366c4569ebb9681e671c04721f9
leonvantuyl <leonvtuyl@hotmail.com>
2014-06-10 02:42:43 +0200

    if om de ports

--- ThreadingEindOpdracht/src/Constanses/ServerConfig.java
+++ ThreadingEindOpdracht/src/Constanses/ServerConfig.java
@@ -3,8 +3,7 @@ package Constanses;
 import java.util.concurrent.Semaphore;
 import threadingeindopdracht.ReadWriteLock;
 
-public class ServerConfig
-  {
+public class ServerConfig {
     //server info
     //public static final int serverIp = localhost;
 
@@ -21,65 +20,63 @@ public class ServerConfig
     public static final String databaseName = "gagpvenn_db2";
     public static final String databaseUsername = "gagpvenn";
     public static final String databasePassword = "123abc456789";
-    
+
     public static ReadWriteLock lock = new ReadWriteLock();
 
-    public static String getInfo()
-      {
+    public static String getInfo() {
         String info = serverPort + "$" + controlPort + "$" + webRoot + "$" + defaultpage + "$" + directoryBrowsing.toString();
         return info;
-      }
+    }
 
-    public static void setInfo(String[] info) throws InterruptedException
-      {
+    public static void setInfo(String[] info) throws InterruptedException {
         lock.lockWrite();
-        serverPort = Integer.parseInt(info[0]);
-        controlPort = Integer.parseInt(info[1]);
+        if (serverPort != Integer.parseInt(info[0])) {
+            serverPort = Integer.parseInt(info[0]);
+            //TODO restart webserver
+        }
+        if (controlPort != Integer.parseInt(info[1])) {
+            controlPort = Integer.parseInt(info[1]);
+            //TODO restart controlserver
+        }
         webRoot = info[2];
         defaultpage = info[3];
         directoryBrowsing = info[4].equalsIgnoreCase("true");
         lock.unlockWrite();
-      }
-    
-    public static int getserverPort() throws InterruptedException
-    {
+    }
+
+    public static int getserverPort() throws InterruptedException {
         lock.lockRead();
         int value = serverPort;
         lock.unlockRead();
         return value;
     }
-    
-    public static int getcontrolPort() throws InterruptedException
-    {
+
+    public static int getcontrolPort() throws InterruptedException {
         lock.lockRead();
         int value = controlPort;
         lock.unlockRead();
         return value;
     }
-    
-    public static String getwebRoot() throws InterruptedException
-    {
+
+    public static String getwebRoot() throws InterruptedException {
         lock.lockRead();
         String value = webRoot;
         lock.unlockRead();
         return value;
     }
-    
-    public static String getdefaultpage() throws InterruptedException
-    {
+
+    public static String getdefaultpage() throws InterruptedException {
         lock.lockRead();
         String value = defaultpage;
         lock.unlockRead();
         return value;
     }
-    
-    public static Boolean getdirectoryBrowsing() throws InterruptedException
-    {
+
+    public static Boolean getdirectoryBrowsing() throws InterruptedException {
         lock.lockRead();
         Boolean value = directoryBrowsing;
         lock.unlockRead();
         return value;
-    } 
-  
+    }
 
-  }
+}
